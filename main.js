ea7677cfe38cef7b7f6986de7190fe8f53293185
Smaranika Guru <smaranikaguru@MacBookAir.home>
2025-11-24 09:13:20 +0100

    this is 1st iteration of complete profile

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const observerOptions = {
             threshold: 0.1,
             rootMargin: '0px 0px -50px 0px'
-        };
+        };``
         
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
